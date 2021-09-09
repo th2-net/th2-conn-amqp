dfaf9749a998a9ce127cf3223febccf47df7b506
Nikita Shaposhnikov <topru123@gmail.com>
2021-09-09 12:10:32 +0300

    Publisher test updated

--- src/test/kotlin/com/exactpro/th2/conn/amqp/MessagePublisherTest.kt
+++ src/test/kotlin/com/exactpro/th2/conn/amqp/MessagePublisherTest.kt
@@ -14,12 +14,16 @@ class MessagePublisherTest {
     @Test
     internal fun `test publisher`() {
         val msg = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Msg></Msg>"
+        val alias = "test-alias"
 
-        val publisher = MessagePublisher("test-alias", 1000, mockRouter)
+        val publisher = MessagePublisher(alias, 1000, mockRouter)
         publisher.onMessage(Direction.FIRST, MessageHolder(msg.toByteArray()))
         Thread.sleep(1050)
         verify(mockRouter).sendAll(check {
-            Assertions.assertEquals(msg, it.getMessages(0).body.toStringUtf8())
+            Assertions.assertEquals(1, it.messagesCount)
+            val rawMessage = it.getMessages(0)
+            Assertions.assertEquals(msg, rawMessage.body.toStringUtf8())
+            Assertions.assertEquals(alias, rawMessage.metadata.id.connectionId.sessionAlias)
         }, check {
             Assertions.assertEquals("first", it)
         })
